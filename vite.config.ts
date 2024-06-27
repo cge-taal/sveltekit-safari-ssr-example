1408f23116ff35a47ecdfb1d483510b05bb7774e
cge-taal <107926963+cge-taal@users.noreply.github.com>
2024-06-27 20:52:36 +0200

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
+  build: {
+    target: "esnext",
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
 });
