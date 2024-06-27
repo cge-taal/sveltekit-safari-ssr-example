e56b8ffc2049dcdd972c4c15e7215e2fdd401b64
cge-taal <107926963+cge-taal@users.noreply.github.com>
2024-06-27 21:00:49 +0200

    Add and use library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script lang="ts">
+  import { encodeBase58Address } from "@bitauth/libauth";
+
+  export function trySomething() {
+    return encodeBase58Address("p2pkh", new Uint8Array());
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
